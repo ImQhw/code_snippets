5fb11409f0c7b1cb7e649616ab983aa5d0458986
qin_hw <1039954093@qq.com>
2021-08-09 16:52:05 +0800

    feat: use insertion_sort when doing median filter

--- median_filtering.py
+++ median_filtering.py
@@ -2,10 +2,32 @@
 from pprint import pprint
 from typing import List
 
-from quick_sort import quick_sort
+
+def insertion_sort(a: List[int]) -> List[int]:
+    if len(a) <= 1:
+        return a
+
+    n = len(a)
+
+    for i in range(1, n):
+        # invariant: a[0...i-1] sorted before insertion
+        # and a[0...i] sorted after insertion
+
+        insert_value = a[i]
+
+        j = i
+        while j > 0 and a[j - 1] > insert_value:
+            # move a[j-1] --> a[j]
+            a[j] = a[j - 1]
+            j -= 1
+
+        # a[j-1] <= insert_value
+        a[j] = insert_value
+
+    return a
 
 
-def median_filtering(array2d: List[List[int]], kernel_size: int, sorter=quick_sort):
+def median_filtering(array2d: List[List[int]], kernel_size: int):
     assert kernel_size & 1, "kernel_size must be odd!"
     assert array2d, "empty array!"
 
@@ -28,7 +50,7 @@ def median_filtering(array2d: List[List[int]], kernel_size: int, sorter=quick_so
         for x in range(half_k, W - half_k):
             neighbors = extract_neighbors(x, y)
 
-            sorted_neighbors = sorter(neighbors)
+            sorted_neighbors = insertion_sort(neighbors)
 
             array2d[y][x] = sorted_neighbors[mid_index]
 
@@ -43,9 +65,11 @@ def main():
     pprint("raw: ")
     pprint(array2d)
 
-    median_filtering(array2d, 3, sorter=quick_sort)
+    median_filtering(array2d, 3)
     pprint("after: ")
     pprint(array2d)
+
+    print(insertion_sort([2, 3, 1]))
 
 
 if __name__ == "__main__":
