68bc3e2d82ab00180339855388c4f98c731d5f43
qin_hw <1039954093@qq.com>
2021-08-11 09:21:58 +0800

    feat: add search_section to binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -71,6 +71,21 @@ def binary_search_last(a: list, t: int) -> int:
     return -1 if (low <= -1 or a[low] != t) else low
 
 
+def search_section(a: list, t):
+    low, high = 0, len(a) - 1
+
+    # invariant: a[low] < t <= a[high]
+    while (low + 1) != high:
+        mid = low + (high - low) // 2
+
+        if t <= a[mid]:
+            high = mid
+        else:  # a[mid] < t
+            low = mid
+
+    return low, high
+
+
 def main():
     assert binary_search_first([1], 1) == 0
     assert binary_search_first([1, 1], 1) == 0
@@ -82,6 +97,13 @@ def main():
     assert binary_search_first([1, 1, 2, 2], 0) == -1
     assert binary_search([1, 1, 2, 2], 0) == -1
     assert binary_search([1, 1, 2, 2], 1) == 1
+
+    print(search_section([1, 2, 3, 4, 5], 2))
+    print(search_section([1, 2, 3, 4, 5], 2.5))
+    print(search_section([1, 2, 3, 4, 5], 3.5))
+    print(search_section([1, 2, 3, 4, 5], 0))
+    print(search_section([1, 2, 3, 4, 5], 5))
+    print(search_section([1, 2, 3, 4, 5], 5.0001))
 
 
 if __name__ == "__main__":
